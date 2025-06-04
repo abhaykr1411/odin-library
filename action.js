0d4011c1c53924b169578b335badf1ed76bb025c
Abhay <actuallyabhay@gmail.com>
2025-06-04 22:41:10 +0000

    form validation done

--- action.js
+++ action.js
@@ -1,36 +1,104 @@
 const myLibrary = [];
 const addBookButton = document.querySelector("[data-open-modal]");
-const closeButton = document.querySelector("[data-close-modal]");
+const closeButton = document.querySelector(".modal-close");
 const modal = document.querySelector("[data-modal]");
 
-addBookButton.addEventListener("click", () =>{
-    modal.showModal();
-});
+//form elements
+const formDOM = document.getElementById("form");
+const titleDOM = document.getElementById("title");
+const authorDOM = document.getElementById("author");
+const pagesDOM = document.getElementById("pages");
+const yearDOM = document.getElementById("year");
+const isReadDOM = document.getElementById("read");
 
-closeButton.addEventListener("click", () =>{
+formDOM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let validInput = validateInput();
+  if(validInput){
     modal.close();
+  }
 });
 
-function Book(BookID, title, author, pages, isRead)
-{
-    if(!new.target){
-        throw Error("You must use the 'new' operator to call the constructor.");
-    }
-    this.BookID = BookID;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
 
-    this.info = function(){
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${(isRead)? "read already": "not read yet"}`);
-    };
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const validateInput = () => {
+  const titleVal = titleDOM.value.trim();
+  const authorval = authorDOM.value.trim();
+  const pagesVal = pagesDOM.value.trim();
+  const yearVal = yearDOM.value.trim();
+  const readVal = isReadDOM.checked;
+
+  if (titleVal === "") {
+    setError(titleDOM, "title is required!");
+    return false;
+  } else {
+    setSuccess(titleDOM);
+  }
+  if (authorval === "") {
+    setError(authorDOM, "Author name is required!");
+    return false;
+  } else {
+    setSuccess(authorDOM);
+  }
+  if (pagesVal === "") {
+    setError(pagesDOM, "Number of pages is required!");
+    return false;
+  } else {
+    setSuccess(pagesDOM);
+  }
+  if (yearVal === "") {
+    setError(yearDOM, "Year of publication is required!");
+    return false;
+  } else {
+    setSuccess(yearDOM);
+  }
+  return true;
+};
+addBookButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
+
+function Book(BookID, title, author, pages, isRead) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor.");
+  }
+  this.BookID = BookID;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+
+  this.info = function () {
+    console.log(
+      `${this.title} by ${this.author}, ${this.pages} pages, ${
+        isRead ? "read already" : "not read yet"
+      }`
+    );
+  };
 }
 
-function addBookToLibrary(title, author, pages, isRead){
-    const bookID = crypto.randomUUID();
-    const bookCreated = new Book(bookID, title, author, pages, isRead);
-    myLibrary.push(bookCreated);
+function addBookToLibrary(title, author, pages, isRead) {
+  const bookID = crypto.randomUUID();
+  const bookCreated = new Book(bookID, title, author, pages, isRead);
+  myLibrary.push(bookCreated);
 }
 addBookToLibrary("the hobbit", "JK", 256, true);
 console.log(myLibrary);

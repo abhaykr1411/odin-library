beb561ef3a7c7c61760859800a909afdbe6325e2
Abhay <actuallyabhay@gmail.com>
2025-06-04 18:32:42 +0000

    initial commit

---
+++ action.js
@@ -0,0 +1,25 @@
+const myLibrary = [];
+
+function Book(BookID, title, author, pages, isRead)
+{
+    if(!new.target){
+        throw Error("You must use the 'new' operator to call the constructor.");
+    }
+    this.BookID = BookID;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+    this.info = function(){
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${(isRead)? "read already": "not read yet"}`);
+    };
+}
+
+function addBookToLibrary(title, author, pages, isRead){
+    const bookID = crypto.randomUUID();
+    const bookCreated = new Book(bookID, title, author, pages, isRead);
+    myLibrary.push(bookCreated);
+}
+addBookToLibrary("the hobbit", "JK", 256, true);
+console.log(myLibrary);
